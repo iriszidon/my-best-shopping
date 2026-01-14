7e58107ec50ae837ce220ef27c8d3b59d41d9707
iriso <iris.opper@finonex.com>
2026-01-14 22:57:16 +0200

    Test find_element method

--- python_tool_shop/tests/test_tool_shop.py
+++ python_tool_shop/tests/test_tool_shop.py
@@ -2,6 +2,7 @@ import allure
 import pytest
 import os
 from page_objects.main_page import MainPage
+from page_objects.base_page import BasePage
 
 
 @allure.description("Test for tool shop website")
@@ -19,3 +20,13 @@ def test_end_to_end_search_filter_add_sum(tool_name, max_price, limit, setup_pla
     cart_page = main_page.open_cart_page(setup_playwright)
     total_price = cart_page.get_total_price()
     cart_page.assert_cart_total_not_exceeds(limit * max_price, len(items_urls), total_price)
+
+
+def test_print_locator(setup_playwright) -> None:
+    base_page = BasePage(setup_playwright)
+    base_url = os.environ.get("BASE_URL")
+    base_page.navigate_to(base_url)
+    main_page = MainPage(setup_playwright)
+    main_page.click_element(main_page.contact_button_ness)
+    main_page.click_element(main_page.home_page_button_ness)
+
