f9b2c7224bd1a2b24b9a3c4806149c54fe6cf73b
iriso <iris.opper@finonex.com>
2026-01-14 14:43:54 +0200

    Add assert

--- python_tool_shop/page_objects/cart_page.py
+++ python_tool_shop/page_objects/cart_page.py
@@ -12,19 +12,19 @@ class CartPage(BasePage):
         super().__init__(page)
         # All locators are defined when an instance of the page is created.
         self.proceed_to_checkout_button = self.page.locator("button.btn.btn-success, [data-test='proceed-1'], //button[text()='Proceed to checkout']")
-        self.cart_total = self.page.locator("td.col-md-2.text-end, [data-test='cart-total']")
+        self.cart_total = self.page.locator("[data-test='cart-total']")
 
 
     @allure.step("get total amount")
     def get_total_price(self) -> float:
         self.wait_for_selector_to_appear(self.cart_total)
         total_amount = self.cart_total.text_content()
+        float_total_amount = float(total_amount[1:])
         take_screenshot(self.page, "shopping_cart")
-        log_message(self, f"Found total amount: {total_amount}$", level=LogLevel.INFO)
-        return float(total_amount)
+        self.logger.info(f"Found total amount: {total_amount}")
+        return float_total_amount
 
-    @allure.step("assert cart total not exceeds")
-    def assert_cart_total_not_exceeds(
-        self, budget_per_item: int, items_count: int
-    ) -> None:
-        assert budget_per_item * items_count <= 300
+    @allure.step("assert cart total does not exceed the item's amount * maximun budget per item")
+    def assert_cart_total_not_exceeds(self, budget_per_item: int, items_count: int, total_budget: float) -> None:
+        assert total_budget <= items_count * budget_per_item, \
+            f"The total amount {total_budget} should be less than {items_count * budget_per_item}"
