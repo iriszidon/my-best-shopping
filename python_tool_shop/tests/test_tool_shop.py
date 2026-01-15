ff76262dfe726f9a2743a1ce7cb11659bf6dd4e4
iriso <iris.opper@finonex.com>
2026-01-15 21:53:59 +0200

    Add navigate_to_page method

--- python_tool_shop/tests/test_tool_shop.py
+++ python_tool_shop/tests/test_tool_shop.py
@@ -15,7 +15,7 @@ def test_end_to_end_search_filter_add_sum(tool_name, max_price, limit, setup_pla
     base_url = os.environ.get("BASE_URL")
     base_page = BasePage(setup_playwright)
     base_page.navigate_to(base_url)
-    main_page = MainPage(setup_playwright)
+    main_page = MainPage(base_page.page)
     items_urls = main_page.search_items_by_name_under_price(tool_name, max_price, limit)
     main_page.add_items_to_cart(items_urls)
     cart_page = main_page.open_cart_page()
@@ -27,7 +27,13 @@ def test_print_locator(setup_playwright) -> None:
     base_page = BasePage(setup_playwright)
     base_url = os.environ.get("BASE_URL")
     base_page.navigate_to(base_url)
-    main_page = MainPage(setup_playwright)
+    main_page = MainPage(base_page.page)
     main_page.click_element(main_page.contact_button)
     main_page.click_element(main_page.home_page_button)
+
+def navigate_to_page(setup_playwright)-> BasePage:
+    base_page = BasePage(setup_playwright)
+    base_url = os.environ.get("BASE_URL")
+    base_page.navigate_to(base_url)
+    return base_page
 
