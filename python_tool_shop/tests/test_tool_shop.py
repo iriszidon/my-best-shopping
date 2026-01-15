7e458af888e2cb07c5de038f89f105c016d7678c
iriso <iris.opper@finonex.com>
2026-01-15 21:40:06 +0200

    Update tag name

--- python_tool_shop/tests/test_tool_shop.py
+++ python_tool_shop/tests/test_tool_shop.py
@@ -6,7 +6,7 @@ from page_objects.base_page import BasePage
 
 
 @allure.description("Test for tool shop website")
-@pytest.mark.good_experimental_test
+@pytest.mark.ness_task
 @pytest.mark.parametrize(
     "tool_name, max_price, limit", [("hammer", 20, 5), ("pliers", 19, 2)]
 )
@@ -22,12 +22,12 @@ def test_end_to_end_search_filter_add_sum(tool_name, max_price, limit, setup_pla
     total_price = cart_page.get_total_price()
     cart_page.assert_cart_total_not_exceeds(limit * max_price, len(items_urls), total_price)
 
-
+@allure.description("Click on 2 elements in the main page")
 def test_print_locator(setup_playwright) -> None:
     base_page = BasePage(setup_playwright)
     base_url = os.environ.get("BASE_URL")
     base_page.navigate_to(base_url)
     main_page = MainPage(setup_playwright)
-    main_page.click_element(main_page.contact_button_ness)
-    main_page.click_element(main_page.home_page_button_ness)
+    main_page.click_element(main_page.contact_button)
+    main_page.click_element(main_page.home_page_button)
 
