8e547c110322c41e5044ddfa52906512e0c9bef8
iriso <iris.opper@finonex.com>
2026-01-15 16:04:10 +0200

    Reorder lines

--- python_tool_shop/tests/test_tool_shop.py
+++ python_tool_shop/tests/test_tool_shop.py
@@ -12,12 +12,13 @@ from page_objects.base_page import BasePage
 )
 def test_end_to_end_search_filter_add_sum(tool_name, max_price, limit, setup_playwright):
     # search a product, filter by price, add to cart
-    main_page = MainPage(setup_playwright)
     base_url = os.environ.get("BASE_URL")
-    main_page.navigate_to(base_url)
+    base_page = BasePage(setup_playwright)
+    base_page.navigate_to(base_url)
+    main_page = MainPage(setup_playwright)
     items_urls = main_page.search_items_by_name_under_price(tool_name, max_price, limit)
     main_page.add_items_to_cart(items_urls)
-    cart_page = main_page.open_cart_page(setup_playwright)
+    cart_page = main_page.open_cart_page()
     total_price = cart_page.get_total_price()
     cart_page.assert_cart_total_not_exceeds(limit * max_price, len(items_urls), total_price)
 
