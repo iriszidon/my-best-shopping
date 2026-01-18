238581ad05713fec776672eea7793872862438af
iriso <iris.opper@finonex.com>
2026-01-18 14:24:54 +0200

    Test login with unique email

--- python_tool_shop/tests/test_tool_shop.py
+++ python_tool_shop/tests/test_tool_shop.py
@@ -2,7 +2,7 @@ import allure
 import pytest
 import os
 
-from helper.utils import take_screenshot
+from helper.utils import take_screenshot, log_message, LogLevel
 from page_objects.main_page import MainPage
 from page_objects.base_page import BasePage
 from page_objects.login_page import LoginPage
@@ -33,7 +33,7 @@ def test_print_locator(setup_playwright) -> None:
     base_page = navigate_to_page(setup_playwright)
     main_page = MainPage(base_page.page)
     main_page.click_element(main_page.contact_button)
-    take_screenshot(main_page, "some_screen_shot")
+    take_screenshot(main_page, "main_page_screen_shot")
     main_page.click_element(main_page.home_page_button)
 
 
@@ -44,9 +44,10 @@ def test_open_login_page(setup_playwright) -> None:
     main_page = MainPage(base_page.page)
     login_page = main_page.open_login_page()
     register_page = login_page.open_register_your_account_page()
-    register_page.perform_registration("Testx", "Testy")
-    take_screenshot(main_page, "some_screen_shot")
-    login_page.perform_login("iriso@finonex.com", "Viva#100")
+    user_email = register_page.generate_timestamp_email()
+    register_page.perform_registration(user_email,"Testx", "Testy")
+    take_screenshot(main_page, "login_page_screen_shot")
+    login_page.perform_login(user_email, "Viva#100")
 
 
 def navigate_to_page(setup_playwright) -> BasePage:
@@ -54,3 +55,5 @@ def navigate_to_page(setup_playwright) -> BasePage:
     base_url = os.environ.get("BASE_URL")
     base_page.navigate_to(base_url)
     return base_page
+
+
